2b374c5d38cdb77b180176eff483b0c68d65a503
ABKAM2023 <149762275+ABKAM2023@users.noreply.github.com>
2024-03-11 12:21:48 +0200

    Add files via upload

--- source/RU/[LR] ExStats GeoIP.cs
+++ source/RU/[LR] ExStats GeoIP.cs
@@ -19,7 +19,7 @@ namespace LevelsRanksModuleExStatsGeoIP;
 [MinimumApiVersion(80)]
 public class LevelsRanksModuleExStatsGeoIP : BasePlugin
 {
-    public override string ModuleName => "[Levels Ranks] ExStats GeoIP";
+    public override string ModuleName => "[LR] ExStats GeoIP";
     public override string ModuleVersion => "1.0";
     public override string ModuleAuthor => "ABKAM designed by RoadSide Romeo & Wend4r";
     public override string ModuleDescription => "A plugin for GeoIP information.";
